efcd1d09fe0f5175d8316eb91c6ed5bb4b6be8f7
Maria Eduarda Bedetti Amorim <e107ti@etec.sp;gov;br>
2022-09-01 20:27:09 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Início: ");
+int inicio = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Fim: ");
+int fim = Convert.ToInt32(Console.ReadLine());
+
+if (fim >= inicio)
+    {
+     int n = inicio;
+
+     while (n <= fim)
+         {
+            Console.Write($"{n} ");
+            n += 1;
+         }
+    }
+else
+    {
+        Console.WriteLine("Não é possível terminar antes de iniciar.");
+    }
